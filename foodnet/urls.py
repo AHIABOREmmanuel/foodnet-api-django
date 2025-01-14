6ab69bfe827d840d23b92ca41215f691c6cf01bc
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-01-14 13:10:42 +0000

    Add python-dotenv to requirements

--- foodnet/urls.py
+++ foodnet/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import routers
 from api.views import*
-from django.contrib.staticfiles import staticfiles_urlpatterns
+
 
 
 router = routers.DefaultRouter()
@@ -37,4 +37,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
     document_root=settings.MEDIA_ROOT)
 
-    urlpatterns +=staticfiles_urlpatterns()
